6d1fa47c2271c66715cb4bfa43aa5431cac78651
Neptali Galue <neptalijose100@gmail.com>
2024-11-26 21:09:55 -0300

    Pagina lista, 1 valor en cada models, falta buscador y introducir valores

--- Pre_Entrega3/DIDs/admin.py
+++ Pre_Entrega3/DIDs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from DIDs.models import DID, Tarifa, Compania
+
 
 # Register your models here.
+admin.site.register(DID)
+admin.site.register(Tarifa)
+admin.site.register(Compania)
--- Pre_Entrega3/DIDs/models.py
+++ Pre_Entrega3/DIDs/models.py
@@ -12,6 +12,7 @@ class Tarifa(models.Model):
     trafico_entrante = models.DecimalField(max_digits=6, decimal_places=4)
     trafico_saliente = models.DecimalField(max_digits=6, decimal_places=4)
     precio_por_numero = models.DecimalField(max_digits=6, decimal_places=4)
+    pais = models.CharField(max_length=30)
 
 class Compania(models.Model):
     direccion = models.CharField(max_length=60)
